45cd2d20e49675c174ab0fd0fa40117ccde97784
Javier Caballero <caballerojavier13@gmail.com>
2017-01-10 23:53:00 -0300

    Primero cambios para incluir login

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,14 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  get 'signup'  => 'users#new'
+
+  get 'login'  => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103115004) do
+ActiveRecord::Schema.define(version: 20170110030807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20170103115004) do
     t.integer  "product_type_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "user_name"
+    t.string   "password_digest"
   end
 
 end
