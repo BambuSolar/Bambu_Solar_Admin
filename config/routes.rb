13cfb1694cedd607a78f41cb08688365d2af1960
Javier Hernán Caballero García <ext_jcaballe@mercadolibre.com>
2017-07-10 17:56:00 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -41,5 +41,6 @@ Rails.application.routes.draw do
   get '/preview/product_types' => 'website/site#product_type_index', as: 'preview_product_type_index'
   get '/preview/product_types/:product_type_id/products' => 'website/site#products_index', as: 'preview_products_index'
   get '/preview/product_types/:product_type_id/products/:id' => 'website/site#product_show', as: 'preview_product_show'
-
+  get '/info/product_types/products/:id' => 'website/site#product_show', as: 'preview_product_info'
+  
 end
