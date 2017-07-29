0c9716637ecfbf4979c04fef9d0d62a055b81957
Javier Hernán Caballero García <caballerojavier13@gmail.com>
2017-07-29 00:25:38 -0300

    Bug Fixing

--- app/controllers/contact_infos_controller.rb
+++ app/controllers/contact_infos_controller.rb
@@ -21,7 +21,7 @@ class ContactInfosController < SecurityController
         format.html { redirect_to contact_info_path, notice: 'Contact info was successfully updated.' }
         format.json { render :index, status: :ok, location: @contact_info }
       else
-        format.html { render edit_contact_info }
+        format.html { render :edit }
         format.json { render json: @contact_info.errors, status: :unprocessable_entity }
       end
     end
@@ -29,6 +29,6 @@ class ContactInfosController < SecurityController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_info_params
-      params.require(:contact_info).permit(:iframe_map, :telephones, :emails, :facebook_link, :mercadolibre_link, :address_text)
+      params.require('/contact_info/edit').permit(:iframe_map, :telephones, :emails, :facebook_link, :mercadolibre_link, :address_text)
     end
 end
